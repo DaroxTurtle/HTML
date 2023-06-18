c2c6014b1089662f265ff6fe4c58505f49f018bb
Hiraeth <127773881+DaroxTurtle@users.noreply.github.com>
2023-06-17 20:49:00 -0700

    qwe

---
+++ Personal/variables.py
@@ -0,0 +1,43 @@
+# variable =    a reusable container for storing a valuue
+#               a variable behaves as if it were the value it contians
+#               Similar to the value X in Algebra 
+
+# 4 Basic Data types
+
+# INTEGER
+#age = 21
+#maximum = 2
+#quantityh = 10
+
+#print(f"You are {age} years old.")
+#print(f'The maximum is {maximum}.')
+#print(f'You would like to buy {quantityh} items.')
+
+# FLOAT
+#gpa = 3.9
+#Idealgpa = 4.7
+#distance = 54.7
+
+#print(f"My baba's gpa is {gpa}.")
+#print(f"My baba's ideal gpa is {Idealgpa}.")
+#print(f"The distance between my house and my baba's house is {distance} miles.")
+
+# STRING
+#name = "Giesel"
+#food = "Kare-kare"
+#email = "Giesel123@gmail.com"
+
+#print(f"Hello, {name}")
+#print(f"You like {food}")
+#print(f"Your email is {email}")
+
+# BOOLEAN 
+#online = True
+#for_sale = False
+#running = True
+
+#print(f"Are you online?: {online}")
+#print(f"Is the item for sale?: {for_sale}")
+#print(f"Game running: {running}")
+
+
